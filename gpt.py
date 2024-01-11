9c8469daee419732a1c01af8d43238bd91214737
SAURABH ALONE <saurabhaloney85@gmail.com>
2024-01-11 18:23:57 +0530

    starting the projrect

--- gpt.py
+++ gpt.py
@@ -3,3 +3,9 @@ import torch
 from numpy import nn
 
 
+# TODO :
+# 1. Make the embedding layer
+# 2. Make the positional encodiing
+# 3. Make the transformer block
+# 4. make the GPT model
+# 5. mak ethe model decoder
